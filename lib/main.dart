0d3b5050fabdc45aa12a73f3737dbbc87dcf81ab
Sourav Ghosh <1SouravGhosh@noreply.github.com>
2020-07-19 12:41:27 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inline_dialogs_app/custom_utils/service_locator.dart';
 import 'package:flutter_inline_dialogs_app/dialogs/manager.dart';
+import 'package:flutter_inline_dialogs_app/dialogs/model.dart';
+import 'package:flutter_inline_dialogs_app/dialogs/service.dart';
 
 void main() {
+  setupLocator();
   runApp(MyApp());
 }
 
@@ -10,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Inline Dialogs Demo',
+      debugShowCheckedModeBanner: false,
       builder: (context, widget) => Navigator(
         onGenerateRoute: (settings) => MaterialPageRoute(
             builder: (context) => DialogManager(
@@ -20,7 +25,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Inline Dialogs Demo'),
     );
   }
 }
@@ -35,11 +40,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  DialogService _dialogService = locator<DialogService>();
   void _incrementCounter() {
+    var _response = _dialogService.showDialog(
+        title: Text("$_counter"),
+        content: null,
+        dialogType: DialogType.confirm,
+        buttonText: "Close");
     setState(() {
       _counter++;
     });
+    print(_response.toString());
   }
 
   @override
